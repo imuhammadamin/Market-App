dd0f11e6326f100e38b93a3c5631a68961d1c537
Javlonbek Rajabov <71313500+radjabov4443@users.noreply.github.com>
2021-12-26 20:09:24 +0500

    update Customer class

--- Market_App/Models/Customer.cs
+++ Market_App/Models/Customer.cs
@@ -8,5 +8,10 @@ namespace Market_App.Models
     internal class Customer : ICustomer
     {
         public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
     }
 }
